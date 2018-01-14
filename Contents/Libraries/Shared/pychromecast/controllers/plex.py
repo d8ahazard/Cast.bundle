b51bdda97f1c3c04e27efeb152232e0a81750652
d8ahazard <d8ahazard@gmail.com>
2018-01-13 22:09:33 -0600

    Add missing methods
    Need to submit this as a PR to the actual module...

--- Contents/Libraries/Shared/pychromecast/controllers/plex.py
+++ Contents/Libraries/Shared/pychromecast/controllers/plex.py
@@ -8,6 +8,10 @@ MESSAGE_TYPE = 'type'
 TYPE_PLAY = "PLAY"
 TYPE_PAUSE = "PAUSE"
 TYPE_STOP = "STOP"
+TYPE_STEPFORWARD = "STEPFORWARD"
+TYPE_STEPBACKWARD = "STEPBACK"
+TYPE_PREVIOUS = "PREVIOUS"
+TYPE_NEXT = "NEXT"
 
 
 class PlexController(BaseController):
@@ -17,14 +21,37 @@ class PlexController(BaseController):
         super(PlexController, self).__init__(
             "urn:x-cast:plex", "9AC194DC")
 
-    def stop(self):
-        """ Send stop command. """
-        self.send_message({MESSAGE_TYPE: TYPE_STOP})
+    def play(self):
+        """ Send play command. """
+        self.send_message({MESSAGE_TYPE: TYPE_PLAY})
 
     def pause(self):
         """ Send pause command. """
         self.send_message({MESSAGE_TYPE: TYPE_PAUSE})
 
-    def play(self):
+    def stop(self):
+        """ Send stop command. """
+        self.send_message({MESSAGE_TYPE: TYPE_STOP})
+
+    def stepforward(self):
         """ Send play command. """
-        self.send_message({MESSAGE_TYPE: TYPE_PLAY})
+        self.send_message({MESSAGE_TYPE: TYPE_STEPFORWARD})
+
+    def stepbackward(self):
+        """ Send stop command. """
+        self.send_message({MESSAGE_TYPE: TYPE_STEPBACKWARD})
+
+    def previous(self):
+        """ Send stop command. """
+        self.send_message({MESSAGE_TYPE: TYPE_PREVIOUS})
+
+    def next(self):
+        """ Send stop command. """
+        self.send_message({MESSAGE_TYPE: TYPE_NEXT})
+
+
+
+
+
+
+
