5d83bae3049e66dd7d106075406f6ee08ff24b11
digitalhigh <d8ahazard@gmail.com>
2018-01-19 19:04:52 -0600

    Make CustomContainer work more like actual Plex containers
    Not sure if it matters, but the goal is to create custom info inside the web UI that matches the proper Plex schema, and another set of endpoints that return data however I need it. Now we can define restrictions on children and attributes if so desired, but still not throw a full error that breaks the process.  :D

--- Contents/Code/CustomContainer.py
+++ Contents/Code/CustomContainer.py
@@ -1,12 +1,19 @@
-from dicttoxml import dicttoxml
+"""
+This is a custom Object Class that we can use to emulate the XML structure of Plex's API output
+For each container type you want to create, specify self.name, and an optional list of
+accpetable attributes.
+
+"""
 
 ObjectClass = getattr(getattr(Redirect, "_object_class"), "__bases__")[0]
 
 class CustomContainer(ObjectClass):
 
 
-    def __init__(self):
+    def __init__(self, attributes=None, children=None):
         ObjectClass.__init__(self, "")
+        self.children = children
+        self.attributes = attributes
         self.SetHeader("Content-Type", "application/xml")
         self.items = []
 
@@ -15,15 +22,40 @@ class CustomContainer(ObjectClass):
         return xml
 
     def add(self,obj):
-        self.items.append(obj)
+        if self.children is None:
+            self.items.append(obj)
+        else:
+            append = False
+            for child in self.children:
+                if obj.name == child:
+                    append = True
+
+            if append is True:
+                self.items.append(obj)
+            else:
+                Log.Error("Child type %s is not allowed" % obj.name)
 
     def to_xml(self):
-        size = str(len(self.items))
         string = ""
-        string += ('<' + self.name + ' size="' + size + '"')
+        string += ('<' + self.name)
+
+        if self.show_size is True:
+            size = str(len(self.items))
+            string += (' size="' + size + '"' )
+
         if self.dict is not None:
             for key, value in self.dict.items():
-                string += (" " + key + '="' + str(value) + '"')
+                allowed = True
+                if self.attributes is not None:
+                    allowed = False
+                    for attribute in self.attributes:
+                        if key == attribute:
+                            allowed = True
+
+                if allowed is True:
+                    string += (" " + key + '="' + str(value) + '"')
+                else:
+                    Log.Error("Attribute " + key + " is not allowed in this container.")
 
         count = len(self.items)
         if count >= 1:
@@ -39,17 +71,57 @@ class CustomContainer(ObjectClass):
         return string
 
 
+# Class to emulate proper Plex media container
 class MediaContainer(CustomContainer):
     def __init__(self, dict=None):
+        self.show_size=True
         self.dict = dict
         self.name="MediaContainer"
         CustomContainer.__init__(self)
 
 
+# Class to emulate proper Plex device container
 class DeviceContainer(CustomContainer):
     def __init__(self, dict=None):
+        self.show_size = False
         self.dict = dict
         self.name="Device"
+        allowed_attributes = [
+            "name",
+            "publicAddress",
+            "product",
+            "productVersion",
+            "platform",
+            "platformVersion",
+            "device",
+            "model",
+            "vendor",
+            "id",
+            "token"
+            "createdAt",
+            "lastSeenAt",
+            "screenResolution",
+            "screenDensity"
+        ]
+
+        allowed_children = [
+            "Connection"
+        ]
+
+        CustomContainer.__init__(self, allowed_attributes, allowed_children)
+
+class CastContainer(CustomContainer):
+    def __init__(self, dict=None):
+        self.show_size = False
+        self.dict = dict
+        self.name="Device"
+        CustomContainer.__init__(self)
+
+class StatusContainer(CustomContainer):
+    def __init__(self, dict=None):
+        self.show_size = False
+        self.dict = dict
+        self.name = "Status"
         CustomContainer.__init__(self)
 
 
